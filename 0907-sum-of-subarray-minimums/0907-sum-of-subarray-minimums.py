1efd06f47bb2188ef0aad3f708b53f99ef9707de
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-10 23:28:33 +0300

    Time: 471 ms (95.92%), Space: 18.7 MB (84.43%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,22 +1,16 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        left = [-1] * len(arr)
-        right = [len(arr)] * len(arr)
-        s = []
-        for i in range(len(arr)):
-            while s and arr[s[-1]] >= arr[i]:
-                right[s.pop()] = i
+        left = 0
+        nums = arr
+        nums.append(float('-inf'))
+        s = [-1]
+        for i in range(len(nums)):
+            while nums[s[-1]] > nums[i]:
+                # print(i)
+                mid = s.pop()
+                interval = ((mid - s[-1]) * (i-mid)) * nums[mid]
+                left += interval
             s.append(i)
-        s = []
-        for i in range(len(arr) -1,-1,-1):
-            while s and arr[s[-1]] > arr[i]:
-                left[s.pop()] = i
-            s.append(i)
-        total = 0
-        # print(left, right)
-        for i in range(len(arr)):
-            # print((i - left[i]),(right[i] - i))
-            ans = ((i - left[i]) * (right[i] - i))*arr[i]
-            total += ans
-        return total % MOD
+        nums.pop()
+        return left%MOD
