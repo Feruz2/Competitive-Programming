b9ad37a64c1a514ed4efd0c3a73f56eb47c2496f
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-22 23:53:22 +0300

    Time: 266 ms (95.46%), Space: 31.7 MB (9.52%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        @cache
+        def rec(idx1,idx2):
+            if idx1 < 0:
+                return idx2 + 1
+            if idx2 < 0:
+                return idx1 + 1
+            
+            if word1[idx1] == word2[idx2]:
+                return rec(idx1 - 1,idx2 - 1)
+            return 1 + min(rec(idx1 - 1,idx2), rec(idx1,idx2 - 1))
+        return rec(len(word1) - 1,len(word2) - 1)
