8d7743b14eb8c955440537dfb9385b467bbd9bf8
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-23 19:57:38 +0300

    Time: 2943 ms (62.08%), Space: 486.8 MB (5.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,41 +4,41 @@ class Solution:
         
         if 0 == amount:
             return 0
-#         @cache
-#         def rec(idx,sofar):
+        @cache
+        def rec(idx,sofar):
 
-#             # if sofar == 0:
-#             #     return 1
-#             if sofar < 0:
-#                 return float("inf")
-#             if idx <= 0:
+            # if sofar == 0:
+            #     return 1
+            if sofar < 0:
+                return float("inf")
+            if idx <= 0:
                 
-#                 if sofar % coins[idx] == 0:
-#                     return ((sofar) // coins[idx])
-#                 return float("inf")
+                if sofar % coins[idx] == 0:
+                    return ((sofar) // coins[idx])
+                return float("inf")
             
-#             return min(1 + rec(idx,sofar - coins[idx]), rec(idx - 1,sofar))
+            return min(1 + rec(idx,sofar - coins[idx]), rec(idx - 1,sofar))
         
-#         ans = rec(len(coins) - 1,amount)
-#         return ans 
-        dp = []
-        for i in range(len(coins)):
-            new_row = []
-            for j in range(amount + 1):
-                new_row.append(0)
-            dp.append(new_row)
+        ans = rec(len(coins) - 1,amount)
+        return ans if ans != float('inf') else -1 
+#         dp = []
+#         for i in range(len(coins)):
+#             new_row = []
+#             for j in range(amount + 1):
+#                 new_row.append(0)
+#             dp.append(new_row)
             
-        for i in range(1,amount+1):
-            if (i) % coins[0] == 0:
-                dp[0][i] = i//coins[0]
-            else:
-                dp[0][i] = float('inf')
+#         for i in range(1,amount+1):
+#             if (i) % coins[0] == 0:
+#                 dp[0][i] = i//coins[0]
+#             else:
+#                 dp[0][i] = float('inf')
                 
-        for i in range(1,len(coins)):
-            for j in range(amount + 1):
-                not_take = dp[i-1][j]
-                take = float('inf')
-                if j >= coins[i]:
-                    take = 1 + dp[i][j-coins[i]]
-                dp[i][j] = min(take,not_take)
-        return dp[-1][-1] if dp[-1][-1] != float('inf') else -1
+#         for i in range(1,len(coins)):
+#             for j in range(amount + 1):
+#                 not_take = dp[i-1][j]
+#                 take = float('inf')
+#                 if j >= coins[i]:
+#                     take = 1 + dp[i][j-coins[i]]
+#                 dp[i][j] = min(take,not_take)
+#         return dp[-1][-1] if dp[-1][-1] != float('inf') else -1
