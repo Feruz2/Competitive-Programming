97184555708027fc3b2ac8aefd7b078c470d8cd8
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-25 15:24:16 +0300

    Time: 1206 ms (46.09%), Space: 22.7 MB (55.76%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -13,23 +13,7 @@ class Solution:
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        # print(dp)
-        """ 
-             c  a  c  a  a  b  a  a  a  a
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        b[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1], 
-        b[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-        c[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        b[0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2], 
-        c[0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2], 
-        a[0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3], 
-        a[0, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4], 
-        b[0, 1, 2, 2, 3, 4, 5, 5, 5, 5, 5], 
-        c[0, 1, 2, 3, 3, 4, 5, 5, 5, 5, 5]]
-
-
-        
-        """
+       
         ans = [""]*((len(str1) + len(str2)) - dp[-1][-1])
         i = len(dp) - 1
         j = len(dp[0])-1
@@ -42,12 +26,12 @@ class Solution:
                 i = i-1
                 j = j -1
             elif dp[i-1][j] >= dp[i][j-1]:
-                    # print(i,j,"up")
+                   
                     ans[z] = str1[i - 1]
                     z -= 1
                     i = i - 1
             else:
-                # print(i,j,"left")
+                
                 ans[z] = str2[j - 1]
                 z -= 1
                 j -= 1
@@ -59,7 +43,7 @@ class Solution:
                 ans[z] = v
                 z -= 1
         if i >= 0:
-            print(i)
+           
             arr = str1[:i]
             arr =arr[::-1]
             for i in arr:
