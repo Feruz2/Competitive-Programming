9d56e22c0104fb02d50a7eef2e18b41381e04c85
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-29 00:16:50 +0300

    Time: 382 ms (86.87%), Space: 24.9 MB (58.33%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -2,18 +2,14 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         nums.sort()
         if len(nums) <= 4:
+            # cause you can make all the numbers the same
             return 0
-        if len(nums) == 5:
-            ans = float('inf')
-            for i in range(len(nums) - 1):
-                ans = min(ans,nums[i + 1] - nums[i])
-            return ans
+
         i = 0
         j = -4
         ans = float('inf')
         k = 0
-        while k < 4:
-            # print(nums[j] - nums[i],j,i)
+        while k <= 3:
             ans = min(ans,nums[j] - nums[i])
             i += 1
             j += 1
