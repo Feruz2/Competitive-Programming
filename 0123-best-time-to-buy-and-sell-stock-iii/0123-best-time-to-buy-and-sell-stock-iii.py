df9ba8d18eb487c3f99404a902efaf5f6ea91402
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-30 23:46:30 +0300

    Time: 5678 ms (13.54%), Space: 27.7 MB (90.58%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -12,27 +12,21 @@ class Solution:
         #         profit = max(prices[idx] + rec(idx + 1, 1, cap - 1), 0 + rec(idx + 1, 0, cap))
         #     return profit
         # return rec(0, 1, 2)
-        dp = []
-        for i in range(len(prices) + 1):
-            new = []
-            for j in range(2):
-                l = []
-                for k in range(2+1):
-                    l.append(0)
-                new.append(l)
-            dp.append(new)
+        dp = [[0,0,0],[0,0,0]]
+        curr = [[0,0,0],[0,0,0]]
 
         for idx in range(len(prices) - 1,-1,-1):
             for canBuy in range(2):
                 for cap in range(2,0,-1):
 
-
                     if canBuy:
-                        profit = max(-prices[idx] + dp[idx + 1][0][cap], 0 + dp[idx + 1][1][cap])
+                        profit = max(-prices[idx] + dp[0][cap], 0 + dp[1][cap])
                     else:
-                        profit = max(prices[idx] + dp[idx + 1][1][cap - 1], 0 + dp[idx + 1][0][cap])
-                    dp[idx][canBuy][cap] = profit
+                        profit = max(prices[idx] + dp[1][cap - 1], 0 + dp[0][cap])
+                    curr[canBuy][cap] = profit
+            dp = curr
+            
         # print(dp)
-        return dp[0][1][2]
+        return dp[1][2]
                 
                 
