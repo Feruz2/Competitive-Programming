f1be396cb28891025ab2fbf6b0848fbb8a89ed4f
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-11-30 22:00:48 +0300

    Time: 234 ms (5.01%), Space: 15.2 MB (69.84%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,27 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-#         @cache
-#         def rec(idx,canBuy):
-            
-#             if idx == len(prices):
-#                 return 0
-#             if canBuy:
-#                 profit = max(-prices[idx] + rec(idx + 1,0), 0 + rec(idx + 1,1))
-#             else:
-#                 profit = max(prices[idx] + rec(idx + 1,1), 0 + rec(idx + 1,0))
-#             return profit
-#         return rec(0,1)
-        dp = []
-        for i in range((len(prices) + 1)):
-            new_row = [0,0]
-            dp.append(new_row)
-                       
+
         
+        dp = [0,0]
+        curr = [0,0]  
         for idx in range(len(prices) - 1, -1, -1):
             for j in range(2):
                 if j:
-                    profit = max(-prices[idx] + dp[idx + 1][0], 0 + dp[idx + 1][1])
+                    profit = max(-prices[idx] + dp[0], 0 + dp[1])
                 else:
-                    profit = max(prices[idx] + dp[idx + 1][1], 0 + dp[idx + 1][0])
-                dp[idx][j] = profit
-        return dp[0][1]       
+                    profit = max(prices[idx] + dp[1], 0 + dp[0])
+                curr[j] = profit
+            dp = curr
+        return dp[1]       
