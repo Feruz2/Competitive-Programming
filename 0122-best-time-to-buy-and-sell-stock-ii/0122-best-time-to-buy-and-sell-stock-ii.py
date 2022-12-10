d5d18a8842ff1d82a2c25570cf12f7ffa2f4ef94
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-12-10 21:05:36 +0300

    Time: 90 ms (75.36%), Space: 17.4 MB (5.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
+        dp = []
+        for i in range((len(prices) + 1)):
+            new_row = [0,0]
+            dp.append(new_row)
+                       
         
-        dp = [0,0]
-        curr = [0,0]  
         for idx in range(len(prices) - 1, -1, -1):
             for j in range(2):
                 if j:
-                    profit = max(-prices[idx] + dp[0], 0 + dp[1])
+                    profit = max(-prices[idx] + dp[idx + 1][0], 0 + dp[idx + 1][1])
                 else:
-                    profit = max(prices[idx] + dp[1], 0 + dp[0])
-                curr[j] = profit
-            dp = curr
-        return dp[1]       
+                    profit = max(prices[idx] + dp[idx + 1][1], 0 + dp[idx + 1][0])
+                dp[idx][j] = profit
+        print(dp)
+        return dp[0][1]
