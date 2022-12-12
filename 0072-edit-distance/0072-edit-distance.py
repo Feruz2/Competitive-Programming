c0a1496a534257f8834681c7b62836bc97dcd925
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-12-13 02:14:15 +0300

    Time: 229 ms (69.21%), Space: 17.7 MB (36.26%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,19 +1,48 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @cache
-        def rec(i,j):
-            if i == len(word1) and j == len(word2):
-                return 0
-            if i == len(word1):
-                return len(word2) - j
-            if j == len(word2):
-                return len(word1) - i
-            if word1[i] == word2[j]:
-                return rec(i + 1, j + 1)
-            delete = 1 + rec(i + 1,j)
-            insert = 1 + rec(i, j + 1)
-            replace = 1 + rec(i + 1, j+1)
-            return min(delete,insert,replace)
-        return rec(0,0)
+        # @cache
+        # def rec(i,j):
+        #     if i == len(word1) and j == len(word2):
+        #         return 0
+        #     if i == len(word1):
+        #         return len(word2) - j
+        #     if j == len(word2):
+        #         return len(word1) - i
+        #     if word1[i] == word2[j]:
+        #         return rec(i + 1, j + 1)
+        #     delete = 1 + rec(i + 1,j)
+        #     insert = 1 + rec(i, j + 1)
+        #     replace = 1 + rec(i + 1, j+1)
+        #     return min(delete,insert,replace)
+        # return rec(0,0)
        
-        
+        rec = [[0]*(len(word2) + 1) for _ in range(len(word1)+1)]
+        for i in range(len(rec)):
+            for j in range(len(rec[i])):
+                if i == len(word1) and j == len(word2):
+                    continue
+                elif i == len(word1):
+                    rec[i][j] = len(word2) - j
+                elif j == len(word2):
+                    rec[i][j] = len(word1) - i
+        # print(rec)
+        """[
+          s  p  a  k  e
+        p[0, 0, 0, 0, 0, 4],
+        a[0, 0, 0, 0, 0, 3],
+        r[0, 0, 0, 0, 0, 2], 
+        k[0, 0, 0, 0, 0, 1], 
+         [5, 4, 3, 2, 1, 0]]
+
+"""
+        for i in range(len(word1) -1,-1,-1 ):
+            for j in range(len(word2) - 1,-1,-1):
+                if word1[i] == word2[j]:
+                    rec[i][j] = rec[i + 1][j + 1]
+                    continue
+                delete = 1 + rec[i + 1][j]
+                insert = 1 + rec[i][j + 1]
+                replace = 1 + rec[i + 1][j+1]
+                rec[i][j] = min(delete,insert,replace)
+       
+        return rec[0][0]
