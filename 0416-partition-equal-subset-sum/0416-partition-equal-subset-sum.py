a4dcf02291f4965c6b2cb290613b96aecf5f2861
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-12-13 00:57:00 +0300

    Time: 4694 ms (25.14%), Space: 496 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,26 +3,46 @@ class Solution:
         summ = sum(nums) // 2
         if sum(nums) % 2 != 0:
             return False
-        dp = []
-        for i in range(len(nums)):
-            new_row = []
-            for j in range(summ + 1):
-                new_row.append(False)
-            dp.append(new_row)
+        @cache
+        def rec(idx,s):
+            nonlocal summ
+            if s == summ:
+                return True
             
-        for i in range(len(nums)):
-            dp[i][0] = True
+            if idx >= len(nums):
+                return False
             
-        if nums[0] <= summ:
-            
-            dp[0][nums[0]] = True
+            return rec(idx + 1,s + nums[idx]) or rec(idx+1,s)
         
-        for i in range(1,len(nums)):
-            for j in range(summ + 1):
-                if nums[i] > j:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j - nums[i]]    
+        
+        
+        return rec(0,0)
+        
+        
+        
+        
+        
+        
+#         dp = []
+#         for i in range(len(nums)):
+#             new_row = []
+#             for j in range(summ + 1):
+#                 new_row.append(False)
+#             dp.append(new_row)
             
-        return dp[-1][-1]
+#         for i in range(len(nums)):
+#             dp[i][0] = True
+            
+#         if nums[0] <= summ:
+            
+#             dp[0][nums[0]] = True
+        
+#         for i in range(1,len(nums)):
+#             for j in range(summ + 1):
+#                 if nums[i] > j:
+#                     dp[i][j] = dp[i-1][j]
+#                 else:
+#                     dp[i][j] = dp[i-1][j] or dp[i-1][j - nums[i]]    
+            
+#         return dp[-1][-1]
         
