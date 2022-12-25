058a2c6191ab2fcfe9082676cd33806bf2c20d9d
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2022-12-25 19:30:56 +0300

    Time: 40 ms (86.49%), Space: 13.9 MB (84.80%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,20 +1,21 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         
-        final = []
-        def rec(idx,ans):
-            if idx == len(s):
-                if len(ans) == 4:
-                    new = ""
-                    for val in ans:
-                        new += val + "."
-                    final.append(new[:-1])
+        ans = []
+        def rec(idx,curr,cnt):
+            if cnt == 0 and idx >= len(s):
+                ans.append(curr[:-1])
                 return 
-            if 0 <= int(s[idx])<= 255:
-                rec(idx+1,ans + [s[idx]])
-            if s[idx] != "0" and idx <= len(s) - 2:
-                rec(idx+2, ans + [s[idx:idx+2]])
-            if idx <= len(s) - 3 and s[idx] != "0" and  0 <= int(s[idx:idx+3]) <= 255:
-                rec(idx+3, ans + [s[idx:idx+3]])
-        rec(0,[])
-        return final
+            if idx >= len(s):
+                return
+            if s[idx] == "0":
+                return rec(idx + 1, curr + s[idx] + ".", cnt - 1)
+            rec(idx + 1, curr + s[idx] + ".",cnt - 1)
+            if len(s) >= idx + 2:
+                rec(idx + 2, curr + s[idx:idx+2] + ".", cnt - 1)
+            if len(s) >= idx + 3 and 0 <= int(s[idx:idx+3]) <= 255:
+                rec(idx + 3 ,curr + s[idx:idx+3] + ".",cnt - 1)
+            
+            
+        rec(0,"",4)
+        return ans
