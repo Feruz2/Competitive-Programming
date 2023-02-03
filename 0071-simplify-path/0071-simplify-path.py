858b041acad86fd18bb75a1e64e2bb6fe7f53047
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2023-02-03 19:33:32 +0300

    Time: 41 ms (47.47%), Space: 14.1 MB (9.14%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,62 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        s = []
+        string  = path
+        sp = path.split('/')
+        path =[]
+        for val in sp:
+            if val != '':
+                path.append(val)
+        print(path)
+        for ch in path:
+            if ch == '..':
+                if s:
+                    s.pop()
+            elif ch == '.':
+                continue
+            else:
+                s.append('/'+ch)
+        return "".join(s) if len(s)>0 else '/'
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         for idx,val in enumerate(path):
+#             if val == '':
+#                 path[idx] = '/'
+#         # print(path)
+
+#         for ch in path:
+        
+#             if s and s[-1] == ch and ch == '/':
+#                 continue
+#             elif s and ch == '..':
+#                 while len(s) > 1 and s[-1] == '/':
+#                     x = s.pop()
+#                 if len(s) > 1:
+#                     s.pop()
+#                 # print(s)
+#             else:
+#                 s.append(ch)
+#                 # print(s)
+#         if len(s) > 1 and s[-1] == '/':
+#             s.pop()
+#         ans = "".join(s)
+#         return ans
