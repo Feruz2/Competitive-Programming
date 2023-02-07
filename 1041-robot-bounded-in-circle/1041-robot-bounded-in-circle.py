1b46b9ed6285a2021877fc89415312bc98b673db
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2023-02-07 23:26:24 +0300

    Time: 36 ms (54.01%), Space: 13.9 MB (61.68%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -2,12 +2,6 @@ class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         x = y = 0
         new = instructions
-        # DIRL = [(1,0),(-1,0),(0,-1),(0,1)]
-        # DIRR = [(1,0),(0,1),(0,-1),(-1,0)]
-        # leftToright = {0:0,
-        #               1:3,
-        #               2:2,
-        #               1:3}
         
         dx = 0
         dy = 1
@@ -15,11 +9,13 @@ class Solution:
             if move == "G":
                 x += dx
                 y +=  dy
+                
             elif move == "L":
                 dx, dy = -dy, dx 
             
             elif move == "R":
                 dx , dy = dy, -dx
+                
         if x == 0 and y == 0:
             return True
         return (dx, dy) != (0,1)
