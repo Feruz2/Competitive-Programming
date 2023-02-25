e7310f1b9fa205ecb67f0bf8811591f98a8b97e3
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2023-02-25 14:48:11 +0300

    Time: 609 ms (47.22%), Space: 15.9 MB (41.67%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,35 +1,30 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        dicit = {x:"ab", y:"ba"}
-        if x > y:
-            pri = dicit[x]
-            sec = dicit[y]
-        else:
-            pri = dicit[y]
-            sec = dicit[x]
-            
-        st = []
-        ans = 0
+        x_string = 'ab'
+        y_string = 'ba'
+
+        if x < y:
+            x,y = y,x
+            x_string,y_string = y_string,x_string
+
+        stack = []
+        points = 0
+
         for i in range(len(s)):
-            if not st or st[-1] != pri[0]:
-                st.append(s[i])
-            elif st[-1] == pri[0] and s[i] == pri[1]:
-                ans += max(x,y)
-                st.pop()
-            elif st[-1] == pri[0] and s[i] != pri[1]:
-                st.append(s[i])
-        # print(st,ans)
-        s = st
-        st = []
-        for i in range(len(s)):
-            if not st or st[-1] != sec[0]:
-                st.append(s[i])
-            elif st[-1] == sec[0] and s[i] == sec[1]:
-                ans += min(x,y)
-                st.pop()
-            elif st[-1] == sec[0] and s[i] != sec[1]:
-                st.append(s[i])
-        return ans
-        
-        
-                
+            if stack and stack[-1] + s[i] == x_string:
+                stack.pop()
+                points += x
+                continue
+
+            stack.append(s[i])
+
+        stack2 = []
+        for i in range(len(stack)):
+            if stack2 and stack2[-1] + stack[i] == y_string:
+                stack2.pop()
+                points += y
+                continue
+
+            stack2.append(stack[i])
+
+        return points
