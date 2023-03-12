f078476f06a72a5a79bce24e83dc219755895768
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2023-03-12 11:01:13 +0300

    Time: 985 ms (38.63%), Space: 29 MB (24.30%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,29 +1,27 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        def search(value):
-            l = 0
-            r = len(nums) - 1
-            best = -1
-            while l <= r:
-                mid = (l + r)//2
-                if nums[mid] > value:
-                    r = mid  - 1
-                elif nums[mid] <= value:
-                    best = mid
-                    l = mid + 1
-            return best
-                    
-        
-        
         nums.sort()
-        print(nums)
-        ans = 0
-        for idx,num in enumerate(nums):
-            val = lower - num
-            left = bisect_left(nums,val,idx+1)
-            val = upper - num
-            right = bisect_right(nums,val,idx+1)
-            
-            ans += right-left
-        return ans
+        cur_upper = 0
+        cur_lowwer = 0
+        count = 0
+
+        for i in range(len(nums)):
+            cur_upper = upper - nums[i]
+            cur_lower = lower - nums[i]
+            """
+            lower = 8 cur = 4
+            upper = 10 cur = 6
+            [0,1,4,4,5,8]    4, 6 
+                l = 2
+                r = 5
+            """
+            left = bisect.bisect_left(nums, cur_lower)
+            right = bisect.bisect_right(nums, cur_upper)
+
+            count += right-left 
+            if left <= i < right:
+                count -= 1
+
+        return count//2
+
                 
