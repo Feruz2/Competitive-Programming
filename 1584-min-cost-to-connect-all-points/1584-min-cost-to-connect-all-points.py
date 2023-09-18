4fc9351284df2237bd9b8f12b860c24b2c4f9ab1
Feruz2 <86770648+Feruz2@users.noreply.github.com>
2023-09-18 23:16:56 +0300

    Time: 2253 ms (30.38%), Space: 81 MB (68.50%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -19,33 +19,11 @@ class Union:
 
 class Solution:
     
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-#         dicit = defaultdict(list)
-        
-#         for i in range(len(points)):
-        
-#             for j in range(len(points)):
-#                 if i != j :
-#                     valx = abs(points[i][0] - points[j][0])
-#                     valy = abs(points[i][1] - points[j][1])
-#                     dicit[i].append((valx+valy,j))
-#             dicit[i].sort()
-     
+    def minCostConnectPoints(self, points: List[List[int]]) -> int: 
         union = Union(len(points))
-#         print(dicit)
         summ = 0
-#         start = points[0] 
-#         for i in range(len(points)):
-#             for j in range(len(dicit[i])):
-#                 if union.find(i) != union.find(dicit[i][j][1]):
-#                     union.unify(i,dicit[i][j][1])
-#                     print(dicit[i][j][0],i,dicit[i][j][1])
-#                     print(union.par)
-#                     summ += dicit[i][j][0]
-#                     # print(summ)
-#                     break
-#         return summ
-            
+        n = 0
+        start = 0
         heap = []
 
         for j in range(1,len(points)):
@@ -53,9 +31,8 @@ class Solution:
             valx = abs(points[0][0] - points[j][0])
             valy = abs(points[0][1] - points[j][1])
             heapq.heappush(heap,(valx+valy,j))
-        # print(heap)
-        n = 0
-        start = 0
+        
+        
         while heap and n < len(points) - 1:
             val, idx  = heappop(heap)
             if union.find(idx) != union.find(start):
