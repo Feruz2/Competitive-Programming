27815aa6d1c2894ec0b34debf15df1cf988cb83b
Feruz Ahmed <86770648+Feruz2@users.noreply.github.com>
2024-12-19 03:00:46 +0100

    Time: 0 ms (100.00%), Space: 12.5 MB (8.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,22 +5,32 @@ class Solution(object):
         :rtype: int
         """
        
-        dp = defaultdict()
-        def rec(idx, summ):
+#         dp = defaultdict()
+#         def rec(idx, summ):
             
-            if idx in dp:
-                return dp[idx]
+#             if idx in dp:
+#                 return dp[idx]
             
-            if idx == 0:
-                return nums[idx]
+#             if idx == 0:
+#                 return nums[idx]
             
-            if idx < 0:
-                return 0 
+#             if idx < 0:
+#                 return 0 
             
             
-            ans = max(rec(idx - 2, summ) + nums[idx] , rec(idx - 1, summ))
+#             ans = max(rec(idx - 2, summ) + nums[idx] , rec(idx - 1, summ))
             
-            dp[idx]  = ans
-            return dp[idx]
+#             dp[idx]  = ans
+#             return dp[idx]
         
-        return rec(len(nums) - 1,0)
+#         return rec(len(nums) - 1,0)
+
+
+        dp = []
+        dp.append(nums[0])
+        for i in range(1, len(nums)):
+            not_take = dp[i - 1]
+            take = nums[i]
+            if i > 1: take += dp [i - 2]
+            dp.append(max(take, not_take))
+        return dp[-1]
