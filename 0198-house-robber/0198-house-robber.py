185947f525798f93ba58662b8239f873d8b1d906
Feruz Ahmed <86770648+Feruz2@users.noreply.github.com>
2024-12-19 02:42:53 +0100

    Time: 0 ms (100.00%), Space: 12.5 MB (8.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,33 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # rec(idx, summ)
-        #     if idx == last:
-        #         return summ
-        #     if idx > last:
-        #         return 0
-        #     ans = float('-inf')
-        #     for j from idx + 2 -> last
-        #         summ += nums[j]
-        #         ans = max (ans, rec(j))
-        #         summ -= nums[j]
-        #     return ans + nums[idx]
+       
         dp = defaultdict()
         def rec(idx, summ):
-            
+            # print(idx, summ)
             if idx in dp:
                 return dp[idx]
+            
             if idx == len(nums) - 1:
                 return nums[idx]
-            if idx >=  len(nums):
-                return 0
             
-            ans = 0
-            for i in range(idx + 2, len(nums)):
-                summ += nums[i]
-                ans = max(ans, rec(i, summ))
-                summ -= nums[i]
-            dp[idx]  = ans + nums[idx]
+            if idx >=  len(nums):
+                return 0 
+            
+            
+            ans = max(rec(idx + 2, summ) + nums[idx] , rec(idx + 1, summ))
+            # print(ans)
+            dp[idx]  = ans
             return dp[idx]
         
         return max(rec(0, 0), rec(1,0))
