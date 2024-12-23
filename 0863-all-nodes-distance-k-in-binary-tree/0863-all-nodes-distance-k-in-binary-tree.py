a911c367e49b74fc838c91af39473ea4db82eae8
Feruz Ahmed <86770648+Feruz2@users.noreply.github.com>
2024-12-23 22:06:46 +0100

    Time: 37 ms (73.12%), Space: 17.9 MB (14.66%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,63 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        
+        def rec(node):
+            # print(node)
+            if not node:
+                return 0
+            if node == target:
+                return 1
+            
+            hasTarget = rec(node.left)
+            # print(hasTarget)
+            if hasTarget > 0:
+                steps = k - hasTarget
+                if steps == 0: 
+                    ans.append(node.val)
+                else:
+                    BFS(node.right, steps - 1)
+                
+                # print(node, hasTarget)
+                return hasTarget + 1
+            
+            hasTarget = rec(node.right)
+            if hasTarget > 0:
+                steps = k - hasTarget
+                if steps == 0: 
+                    ans.append(node.val)
+                else:
+                    BFS(node.left, steps - 1)
+                
+                # print(node, hasTarget)
+                return hasTarget + 1
+            return 0
+        
+        ans = []
+        
+        def BFS(node, k):
+            
+            if node:
+                qu = deque([node])
+            else: 
+                qu= []
+            while qu and k >= 0:
+                N = len(qu)
+                for i in range(N):
+                    newNode = qu.popleft()
+                    
+                    if k == 0: ans.append(newNode.val)
+                    if newNode.left: qu.append(newNode.left)
+                    if newNode.right: qu.append(newNode.right)
+                
+                k -= 1
+          
+        rec(root)
+        BFS(target, k)
+        return ans
