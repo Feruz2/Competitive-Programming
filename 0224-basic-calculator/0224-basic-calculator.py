d8eec13b754f2e80a904965c571ee396afe2a5db
Feruz Ahmed <86770648+Feruz2@users.noreply.github.com>
2024-12-26 20:03:14 +0100

    Time: 55 ms (22.31%), Space: 18.9 MB (14.12%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -4,7 +4,7 @@ class Solution:
         ans = 0
         i = len(string) - 1
         while i >= 0:
-            # print(st,i, string[i])
+           
             if string[i] == " ":
                 i -= 1
                 continue
@@ -17,17 +17,8 @@ class Solution:
                     num = new
                     j -= 1
                 st.append(num)
-                # while j >= 0 and string[j] == " ":
-                #     j -= 1
-                # # print(j, "j")
-                # if j == -1: 
-                #     st.append(num)
-                # elif string[j] == "+" or string[j] == "(" or string[j] == ")": 
-                #     st.append(num)
-                # elif string[j] == "-":
-                #     st.append("-" + num)
+                
                 i = j
-                # print(i, st)
             elif string[i] == ")":
                 i -= 1
                 st.append(")")
@@ -52,14 +43,14 @@ class Solution:
                         break
                 
                 st.append(str(int(curr) * - 1))
-                # print(st, "end")
+                
                 while temp:
                     st.append(temp.pop())
                 i -= 1
                 
             else:
                 i -= 1
-        print(st)
+      
         ans = 0
         while st:
             ans += int(st.pop())
